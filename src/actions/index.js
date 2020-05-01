9c4384354b02b4725b291293605d3dcea81d9aa2
Jyothi Turpu <jyothi.turpu@gmail.com>
2020-05-01 18:54:19 -0400

    Introduced Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,19 @@
+import { SET_RECENT_POSTS } from './types';
+import Axios from 'axios';
+
 export function fetchRecentPosts() {
   return function(dispatch) {
     console.log('Hello');
+    Axios.get('https://api.dailysmarty.com/posts')
+    .then(response => {
+      console.log('Response after fetching records ', response.data.posts);
+      dispatch({
+        type: SET_RECENT_POSTS,
+        payload: response.data.posts
+      });
+    })
+    .catch(error => {
+      console.log('Error in fetchRecentPosts=>', error);
+    });
   }
 }
